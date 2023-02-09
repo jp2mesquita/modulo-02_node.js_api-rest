8b5551deddfd81ba7ff649e90e0bf292e362c489
João Paulo Mesquita <jp-mesquita@live.com>
2023-02-09 16:49:54 -0300

    feat: implements routes to fetch data from db

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -21,4 +21,38 @@ export async function transactionsRoutes(app: FastifyInstance) {
 
     return res.status(201).send()
   })
+
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return {
+      transactions,
+    }
+  })
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first()
+
+    return {
+      summary,
+    }
+  })
+
+  app.get('/:id', async (req, res) => {
+    const getTransactionsParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const params = getTransactionsParamsSchema.parse(req.params)
+
+    const { id } = params
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return {
+      transaction,
+    }
+  })
 }
