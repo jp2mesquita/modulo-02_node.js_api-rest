c3d980ed9d03887827c0194ad3d736f450ad3678
João Paulo Mesquita <jp-mesquita@live.com>
2023-02-09 15:10:33 -0300

    feat: implement route to add a new transaction to database

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,9 @@ import { transactionsRoutes } from './routes/transactions'
 
 const app = fasfify()
 
-app.register(transactionsRoutes)
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
+})
 
 app
   .listen({
